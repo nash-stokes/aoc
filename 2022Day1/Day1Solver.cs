1d26afedadcb3c8c0003d3c87b7bda56dfe53c96
Nash Stokes <nashstokes5959@gmail.com>
2023-04-08 20:35:16 -0500

    finished day1

--- 2022Day1/Day1Solver.cs
+++ 2022Day1/Day1Solver.cs
@@ -1,57 +1,77 @@
 using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using AoCProblemSolvers.Utilities;
 
 namespace AoCProblemSolvers._2022Day1;
 
 public class Day1Solver
 {
+    private readonly FileReader _fileReader;
+
+    private readonly IEnumerable<string> _rawText;
+    private readonly string[] _text;
+
+    public Day1Solver()
+    {
+        _fileReader = new FileReader();
+        _rawText = _fileReader.Read("../../../2022Day1/input.txt");
+        _text = _rawText.ToArray();
+    }
     public void SolvePartOne()
     {
-        var fileReader = new FileReader();
-        var rawText = fileReader.Read("../../../2022Day1/input.txt");
-        int sum = 0;
-        List<int> caloriesList = new();
-        foreach (var item in rawText)
+        var sum = 0;
+        var max = 0;
+        foreach(var line in _text)
         {
-            if (item != "")
-            {
-                sum += Int32.Parse(item);
+            if (string.IsNullOrWhiteSpace(line)) { 
+                if (sum > max)
+                {
+                    max = sum;
+                }
+                sum = 0;
             }
             else
             {
-                caloriesList.Add(sum);
-                sum = 0;
+                sum += int.Parse(line);
             }
         }
-        Console.Write(caloriesList.Max());
+        Console.WriteLine("Max calories: " + max);
     }
+
     public void SolvePartTwo()
     {
-        var fileReader = new FileReader();
-        var rawText = fileReader.Read("../../../2022Day1/input.txt");
-        int sum = 0;
-        List<int> caloriesList = new();
-        foreach (var item in rawText)
+        var sum = 0;
+        var max = 0;
+        var two = 0;
+        var three = 0;
+        foreach (var line in _text)
         {
-            if (item != "")
+            if (string.IsNullOrWhiteSpace(line))
             {
-                sum += Int32.Parse(item);
+                if (sum > max)
+                {
+                    three = two;
+                    two = max;
+                    max = sum;
+                }
+                else if(sum > two)
+                {
+                    three = two;
+                    two = sum;
+                }
+                else if(sum > three)
+                {
+                    three = sum;
+                }
+                sum = 0;
             }
             else
             {
-                caloriesList.Add(sum);
-                sum = 0;
+                sum += int.Parse(line);
             }
         }
-
-        sum = 0;
-        for (var i = 0; i < 3; i++)
-        {
-            var currentMax = caloriesList.Max();
-            sum += currentMax;
-            caloriesList.Remove(currentMax);
-        }
-        Console.Write(sum);
+        Console.WriteLine("Top three calories: " + (max + two + three));
     }
+
 }
